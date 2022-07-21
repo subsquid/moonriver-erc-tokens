ed3dd234c64af80a5dfaeae3f60140e27c9849ba
mckrava <mckrava2@gmail.com>
2022-07-21 18:22:50 +0300

    fix: add additional check for transfer handling

--- src/processor.ts
+++ src/processor.ts
@@ -13,9 +13,6 @@ import { entitiesManager } from './mappings/entityUtils';
 
 const database = new TypeormDatabase();
 const processor = new SubstrateBatchProcessor()
-    // .setBlockRange({ from: 179689 })
-    // .setBlockRange({ from: 179689 })
-    // .setBlockRange({ from: 263764 })
     .setBatchSize(config.batchSize)
     .setDataSource({
       chain: config.chainNode,
@@ -49,7 +46,11 @@ processor.run(database, async (ctx: Context) => {
           try {
             await modules.handleErc20Transfer(ctx, block.header, item.event);
           } catch (e) {
-            await modules.handleErc721Transfer(ctx, block.header, item.event);
+            try {
+              await modules.handleErc721Transfer(ctx, block.header, item.event);
+            } catch (error) {
+              console.log(error);
+            }
           }
         }
       }
